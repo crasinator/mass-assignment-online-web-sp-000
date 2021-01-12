380466852f51384702bd2ca662d36a7262696c39
Jordan Reed <jordanreed03@gmail.com>
2021-01-12 00:35:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,9 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
-    @name = name 
-    @hair_color = hair_color
-    @birthday = birthday 
-    @eye_color = eye_color 
-    @height = height 
-    @weight = weight
-    @handed = handed 
-    @complexion = complexion
-    @t_shirt_size = t_shirt_size
-    @wrist_size = wrist_size
-    @glove_size = glove_size 
-    @pant_length = pant_length 
-    @pant_width = pant_width
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
   end 
 end
