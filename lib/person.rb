5789266d0c7fb21fb3374f8d754662267f294a88
Jordan Reed <jordanreed03@gmail.com>
2021-01-12 00:23:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,6 @@
 class Person
 
-  def initialize(:name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width)
+  def initialize(name:)
     @name = name 
     @hair_color = hair_color
   end 
